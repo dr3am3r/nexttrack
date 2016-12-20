25bc30963f9d1d32d402305c3d3e92787c4e30a0
Slava Lebedev <vlebedev.ads@gmail.com>
2016-12-21 01:46:45 +0200

    check this

--- src/app/soundcloud/widget.component.ts
+++ src/app/soundcloud/widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { window } from '@angular/platform-browser/src/facade/browser';
 import '../../3rd-party/sc.api.js';
 import './soundcloud.css';
@@ -7,27 +7,35 @@ import './soundcloud.css';
     selector: 'soundcloud-widget',
     template: `
         <div class="widget-container">
-            <iframe class="iframe" id="sc-widget" width="100%" height="465" scrolling="no" frameborder="no"></iframe>
+            <iframe id="sc-widget" width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/4087864&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
         </div>
     `
 })
-export class SoundcloudComponent {
+export class SoundcloudComponent implements AfterViewInit {
     @Input() trackpath: string;
     private _widget: any;
     private _isReady = false;
 
+    constructor() {
+        //new SC.Widget('sc-widget');
+    }
+
     ngOnInit() {
-        this._widget = SC.Widget('sc-widget');
+        console.log('@ngOnInit widgetComponent');
+    }
+
+    ngOnChanges(changeRecords: any) {
+        console.log('@ngOnChanges', changeRecords);
+        if (this._isReady) {
+            this.trackpath && this._widget.load(this.trackpath)
+        }
+    }
+    ngAfterViewInit() {
+        console.log('@ngAfterViewInit widgetComponent');
+        this._widget = new SC.Widget('sc-widget');
         this._widget.bind(SC.Widget.Events.READY, () => {
             this._isReady = true;
             this.trackpath && this._widget.load(this.trackpath)
         })
-    }
-
-    ngOnChanges(changeRecords: any) {
-        console.log('@ngOnChanges', changeRecords)
-        if (this._isReady) {
-            this.trackpath && this._widget.load(this.trackpath)
-        }
     }
 }
