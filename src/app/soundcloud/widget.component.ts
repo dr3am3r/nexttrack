51a063b51b2cc292428c0f6f61e06b3664c03186
Slava Lebedev <vlebedev.ads@gmail.com>
2016-12-21 16:27:56 +0200

    examples

--- src/app/soundcloud/widget.component.ts
+++ src/app/soundcloud/widget.component.ts
@@ -3,17 +3,21 @@ import { window } from '@angular/platform-browser/src/facade/browser';
 import '../../3rd-party/sc.api.js';
 import './soundcloud.css';
 
+var _widget;
+
 @Component({
     selector: 'soundcloud-widget',
     template: `
         <div class="widget-container">
             <iframe id="sc-widget" width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/4087864&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
         </div>
+        <br><br>
+        <button (click)="onPause($event)">Pause!</button>&nbsp;&nbsp;&nbsp;<button (click)="onPlay($event)">Play!</button>&nbsp;&nbsp;&nbsp;<button (click)="onNext($event)">Next!</button>&nbsp;&nbsp;&nbsp;
+        <br><br>
     `
 })
 export class SoundcloudComponent implements AfterViewInit {
     @Input() trackpath: string;
-    private _widget: any;
     private _isReady = false;
 
     constructor() {
@@ -26,16 +30,36 @@ export class SoundcloudComponent implements AfterViewInit {
 
     ngOnChanges(changeRecords: any) {
         console.log('@ngOnChanges', changeRecords);
-        if (this._isReady) {
-            this.trackpath && this._widget.load(this.trackpath)
-        }
+        //if (this._isReady) {
+        //    this.trackpath && this._widget.load(this.trackpath)
+        //}
     }
     ngAfterViewInit() {
         console.log('@ngAfterViewInit widgetComponent');
-        this._widget = new SC.Widget('sc-widget');
-        this._widget.bind(SC.Widget.Events.READY, () => {
-            this._isReady = true;
-            this.trackpath && this._widget.load(this.trackpath)
+        _widget = SC.Widget('sc-widget');
+        _widget.bind(SC.Widget.Events.READY, () => {
+            _widget.bind(SC.Widget.Events.PLAY, function() {
+                // get information about currently playing sound
+                _widget.getVolume(function(volume) {
+                    console.log('current volume value is ' + volume);
+                });
+            });
+            //this._isReady = true;
+            //this.trackpath && this._widget.load(this.trackpath)
         })
     }
+
+    onPlay(e) {
+        _widget.play();
+    }
+
+    onPause(e) {
+        _widget.pause();
+    }
+
+    onNext(e) {
+        _widget.next();
+    }
+
+
 }
